d55ace787003c42ed00974cefe5b483a9c5e9362
meokisama <hoangdinhsang123@gmail.com>
2024-03-17 22:25:48 +0700

    add mỏe duplicate cases

--- index.js
+++ index.js
@@ -17,6 +17,26 @@ async function fetchData(url) {
             else if (seriesNameText.includes("Sword Art Online")) seriesNameText = "Sword Art Online";
             else if (seriesNameText.includes("Cô gái Văn Chương") || seriesNameText.includes("Cô Gái Văn Chương")) seriesNameText = "Cô Gái Văn Chương";
             else if (seriesNameText.includes("Suzumiya Haruhi")) seriesNameText = "Suzumiya Haruhi";
+            else if (seriesNameText.includes("Infinite Dendrogram")) seriesNameText = "Infinite Dendrogram";
+            else if (seriesNameText.includes("Arifureta - Từ Tầm Thường Đến Bất Khả Chiến Bại")) seriesNameText = "Arifureta - Từ Tầm Thường Đến Bất Khả Chiến Bại";
+            else if (seriesNameText.includes("Cop Craft - Cảnh Sát Đến Từ Hai Thế Giới")) seriesNameText = "Cop Craft - Cảnh Sát Đến Từ Hai Thế Giới";
+            else if (seriesNameText.includes("Okitegami Kyoko")) seriesNameText = "Okitegami Kyoko";
+            else if (seriesNameText.includes("86")) seriesNameText = "86 Eighty Six";
+            else if (seriesNameText.includes("Tập Tỏ Tình")) seriesNameText = "Tập Tỏ Tình";
+            else if (seriesNameText.includes("SPY ROOM - Lớp Học Điệp Viên")) seriesNameText = "SPY ROOM - Lớp Học Điệp Viên";
+            else if (seriesNameText.includes("Tiệm Sách Cũ Biblia")) seriesNameText = "Tiệm Sách Cũ Biblia";
+            else if (seriesNameText.includes("Văn Hào Lưu Lạc")) seriesNameText = "Văn Hào Lưu Lạc";
+            else if (seriesNameText.includes("Cuộc Nổi Dậy Của Cô Nàng Mọt Sách")) seriesNameText = "Cuộc Nổi Dậy Của Cô Nàng Mọt Sách";
+            else if (seriesNameText.includes("Date A Live")) seriesNameText = "Date A Live";
+            else if (seriesNameText.includes("Pháo Hoa, Ngắm Từ Dưới Hay Bên Cạnh")) seriesNameText = "Pháo Hoa, Ngắm Từ Dưới Hay Bên Cạnh";
+            else if (seriesNameText.includes("Đứa Con Của Thời Tiết")) seriesNameText = "Đứa Con Của Thời Tiết";
+            else if (seriesNameText.includes("Khi Hikaru Còn Trên Thế Gian Này")) seriesNameText = "Khi Hikaru Còn Trên Thế Gian Này";
+            else if (seriesNameText.includes("Death Note")) seriesNameText = "Death Note";
+            else if (seriesNameText.includes("5 Centimet Trên Giây")) seriesNameText = "5 Centimet Trên Giây";
+            else if (seriesNameText.includes("Rừng Taiga")) seriesNameText = "Rừng Taiga";
+            else if (seriesNameText.includes("Vì Con Gái Tôi Có Thể Đánh Bại Cả Ma Vương")) seriesNameText = "Vì Con Gái Tôi Có Thể Đánh Bại Cả Ma Vương";
+            else if (seriesNameText.includes("Another")) seriesNameText = "Another";
+            else if (seriesNameText.includes("Your Name")) seriesNameText = "Your Name";
 
             let publisherName = $(element).find('.publisher-name a').text().trim();
             let backgroundImageUrl = $(element).find('.content.img-in-ratio').css('background-image');
